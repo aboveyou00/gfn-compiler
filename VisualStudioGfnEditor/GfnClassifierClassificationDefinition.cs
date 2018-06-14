750a218196792c1939839a2950a7592bc20fd269
Brandon Slade <brandonyoyoslade@gmail.com>
2018-06-13 21:29:30 -0600

    feat(vsx): add visual studio syntax-highlighting extension

---
+++ VisualStudioGfnEditor/GfnClassifierClassificationDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VisualStudioGfnEditor
+{
+    /// <summary>
+    /// Classification type definition export for GfnClassifier
+    /// </summary>
+    internal static class GfnClassifierClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "GfnClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("GfnClassifier")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
---
+++ VisualStudioGfnEditor/GfnFileAndContentTypeDefinitions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VisualStudioGfnEditor
+{
+    /// <summary>
+    /// File and content type definition exports
+    /// </summary>
+    internal class GfnFileAndContentTypeDefinitions
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        [Export(typeof(ContentTypeDefinition))]
+        [Name("gfn")]
+        [BaseDefinition("code")]
+        internal static ContentTypeDefinition contentTypeDefinition;
+
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [FileExtension(".gfn")]
+        [ContentType("gfn")]
+        internal static FileExtensionToContentTypeDefinition fileExtensionToContentTypeDefinition;
+
+#pragma warning restore 169
+    }
+}
